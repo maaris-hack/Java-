61d531bab5c3c35a1bcc74e6575624ffec9c438c
Maroti Haibate <47947677+maaris-hack@users.noreply.github.com>
2024-09-29 00:51:53 +0530

    Create JavaEnd-of-file.java

---
+++ JavaEnd-of-file.java
@@ -0,0 +1,26 @@
+//Code by Maari0126
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+         Scanner sc = new Scanner(System.in);
+
+  int numberline = 1;
+    String n = null;
+    while (sc.hasNextLine()) {
+        n = sc.nextLine();
+        System.out.println(numberline + " " + n);
+
+        numberline++;
+
+
+    }
+    }
+}
